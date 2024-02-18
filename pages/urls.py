18e4c28a7629c68db120c447cd73d8aab7d4b844
Pavel Volchenko <2793553@gmail.com>
2024-02-18 07:52:37 +0300

    base functions accounts: login, logout, signup

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import HomePageView
 
+
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
 ]
--- src/settings.py
+++ src/settings.py
@@ -90,8 +90,11 @@ TIME_ZONE = 'Europe/Moscow'
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'home'
