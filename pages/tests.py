375c8730e0c5f44971a414f03d41f42da6e2a40e
Pavel Volchenko <2793553@gmail.com>
2024-02-18 07:00:21 +0300

    accounts signup form

--- pages/tests.py
+++ pages/tests.py
@@ -19,9 +19,9 @@ class HomepageTests(SimpleTestCase):
         self.assertContains(self.response, 'home page')
 
     def test_homepage_does_not_contain_incorrect_html(self):
-        self.assertNotContains(self.response, 'Меня здесь не должно было быть!')
+        self.assertNotContains(self.response,
+                               'Hi there! I should not be on the page.')
 
     def test_homepage_url_resolves_homepageview(self):
         view = resolve('/')
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
-
